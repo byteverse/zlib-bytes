ac8939b37d00711586379dffad45fdd5bb5ebdbd
Andrew Martin <andrew.thaddeus@gmail.com>
2020-02-04 10:21:29 -0500

    Use HsInt and int appropriately in the C wrapper functions

--- cbits/hs_zlib.c
+++ cbits/hs_zlib.c
@@ -1,26 +1,27 @@
+#include "Rts.h"
 #include "zlib.h"
 
-int hs_initDecompress
+HsInt hs_initDecompress
   ( z_stream *st
   , char *src // source buffer of compressed bytes
-  , int src_len // number of bytes available to be read from src
+  , HsInt src_len // number of bytes available to be read from src
   ) {
   st->zalloc = Z_NULL;
   st->zfree = Z_NULL;
   st->opaque = Z_NULL;
   st->next_in = src;
-  st->avail_in = src_len;
-  return inflateInit(st);
+  st->avail_in = (int)src_len;
+  return (HsInt)(inflateInit(st));
 }
 
-int hs_decompressChunk
+HsInt hs_decompressChunk
   ( z_stream *st
   , char *dst // output buffer for decompressed bytes
-  , int dst_len // number of bytes available to write into dst
+  , HsInt dst_len // number of bytes available to write into dst
   ) {
-  st->avail_out = dst_len;
+  st->avail_out = (int)dst_len;
   st->next_out = dst;
-  return inflate(st, Z_NO_FLUSH);
+  return (HsInt)(inflate(st, Z_NO_FLUSH));
 }
 
-int hs_avail_out(z_stream *st) { return st->avail_out; }
+HsInt hs_avail_out(z_stream *st) { return (HsInt)(st->avail_out); }
